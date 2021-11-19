6e1e1237ca27d0618699fc56d9a8b5996c95fb93
Nemanja Katava <katavanemanja99@gmail.com>
2021-11-19 22:46:36 +0100

    dodat login activity

--- app/src/main/java/com/example/projekat_android/MainActivity.java
+++ app/src/main/java/com/example/projekat_android/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.projekat_android;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button kupac = findViewById(R.id.btnKupac);
+        Button prodavac = findViewById(R.id.btnProdavac);
+
+        kupac.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+
+            }
+        });
+
+        prodavac.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
+
+
